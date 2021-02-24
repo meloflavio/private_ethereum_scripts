e04b4fe58daf12e4885a10b4cefcf5ef9ad72584
Flavio Melo <meloflavio@gmail.com>
2021-02-24 15:49:03 -0300

    unificar scripts

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
+
+#Define parâmetros iniciais
 OPERATIONTYPE=start
 NETWORKID=1288
 VERSION=1.9.20-979fc968
 NODETYPE=node
-DATADIR=$HOME/.ethereum/private/$NODETYPE
+DATADIR=""
 MYNODEPORT=30303
-BOOTNODEIP=192.168.1.115
+
+BOOTNODEKEY=8b7aaa22a34451deac5d53b511cc66a5607c048bfd689ab711a8845cfd254421
+BOOTNODEIP=127.0.0.1
 BOOTNODEPORT=30301
 BOOTNODEID=4e87faaa0ed677c3ec389f3ac37f8b0e366876f73e72764e3518031daca322768befb783be5c4aea4200f3439f4361571e860c38776142094adc35913964096b
 
@@ -14,32 +18,38 @@ BOOTNODEID=4e87faaa0ed677c3ec389f3ac37f8b0e366876f73e72764e3518031daca322768befb
 UNZIP="tar -xvzf"
 IPC=''
 
-while getopts v:t:o:i:d:p:b:r:n: flag
+#Define flags para substituir os parâmetros iniciais
+while getopts t:v:n:d:k:i:p:b:o:m: flag
 do
     case "${flag}" in
-        v) VERSION=${OPTARG};;
         t) NODETYPE=${OPTARG};;
-        o) OPERATIONTYPE=${OPTARG};;
-        i) BOOTNODEIP=${OPTARG};;
-        d) DATADIR=${OPTARG};;
-        p) MYNODEPORT=${OPTARG};;
-        b) BOOTNODEID=${OPTARG};;
-        r) BOOTNODEPORT=${OPTARG};;
+        v) VERSION=${OPTARG};;
         n) NETWORKID=${OPTARG};;
+        d) DATADIR=${OPTARG};;
+        k) BOOTNODEKEY=${OPTARG};;
+        i) BOOTNODEIP=${OPTARG};;
+        p) BOOTNODEPORT=${OPTARG};;
+        b) BOOTNODEID=${OPTARG};;
+        o) OPERATIONTYPE=${OPTARG};;
+        m) MYNODEPORT=${OPTARG};;
     esac
 done
 
+#Se datadir não definido pela flag, define datadir padrão
+if [[ "$DATADIR" == "" ]]; then
+DATADIR=$HOME/.ethereum/private/$NODETYPE
+fi
 
-
+#Define password e chave privada do nó de aplicacao ou minerador
 accountFile=".accountpassword"
 privateFile=".privatekey"
-if [[ "$NODETYPE" == *"e2" ]]; then
+if [[ "$NODETYPE" == "mine"* ]]; then
     accountFile=".accountpassword2"
     privateFile=".privatekey2"
-    DATADIR=$DATADIR"2"
     MYNODEPORT=$(($MYNODEPORT+10))
 fi
 
+#Verificar sistema operacional e definir url das ferramentas da plataforma Ethereum e metodo de descompressão
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     OS='linux'
     EXT='tar.gz'
@@ -56,9 +66,12 @@ else
     IPC='--ipcdisable'
 fi
 
+#Monta URL do arquivo binário
 FILE=geth-alltools-$OS-amd64-$VERSION
 FILEEXT=$FILE.$EXT
 URL=https://gethstore.blob.core.windows.net/builds/$FILEEXT
+
+#Verifica se o arquivo binário ká existe se não inicia download
 if [  -f "$FILEEXT" ]; then
     echo 'Arquivo encontrado'
 else
@@ -72,6 +85,7 @@ fi
 
 
 if [[ "$OPERATIONTYPE" == "start" ]]; then
+#Verifica se o blocco inicial foi criado, caso contrário o inicia
     if [  -d "$DATADIR" ]; then
         echo 'Genesis iniciada'
     else
@@ -79,22 +93,27 @@ if [[ "$OPERATIONTYPE" == "start" ]]; then
         sleep 3
     fi
 
-    output=$($FILE/geth  --verbosity=0 --datadir=$DATADIR account list)
-    
-    echo $output
-    if [[ "$output" == "Account #0"* ]]; then
-        echo 'Conta #0 presente'
-    else
-        $FILE/geth --datadir=$DATADIR account import --password $accountFile $privateFile
-        output=$($FILE/geth  --verbosity=0 --datadir=$DATADIR account list)
+
+    #Se não é nó de boot, pega o endereço da carteira base do nó
+    if [[ "$NODETYPE" != "boot" ]]; then
+      output=$($FILE/geth  --verbosity=0 --datadir=$DATADIR account list)
+      if [[ "$output" == "Account #0"* ]]; then
+          echo 'Conta #0 presente'
+      else
+          $FILE/geth --datadir=$DATADIR account import --password $accountFile $privateFile
+          output=$($FILE/geth  --verbosity=0 --datadir=$DATADIR account list)
+      fi
+      sleep 3
+      adressAccount=$(echo $output | $GREP "\{([^}]+)\}" | $GREP "\w+")
     fi
-    sleep 3
-    adressAccount=$(echo $output | $GREP "\{([^}]+)\}" | $GREP "\w+")
-    echo $NETWORKID
+
+    #Verifica qual o nó vai ser iniciado e executa o comando referente a esse nó
     if [[ "$NODETYPE" == "node"* ]]; then
-        $FILE/geth --nousb --datadir=$DATADIR --syncmode 'full' --bootnodes "enode://$BOOTNODEID@$BOOTNODEIP:$BOOTNODEPORT" --networkid $NETWORKID --port $MYNODEPORT --http --http.addr 'localhost' --http.port 8545 --http.api admin,eth,miner,net,txpool,personal,web3  --allow-insecure-unlock --unlock "0xfd96fcc76da5e04604270bac93cd0e2acdcd670d" --password .accountpassword
+        $FILE/geth --nousb --datadir=$DATADIR --syncmode 'full' --bootnodes "enode://$BOOTNODEID@$BOOTNODEIP:$BOOTNODEPORT" --networkid $NETWORKID --port $MYNODEPORT --http --http.addr 'localhost' --http.port 8545 --http.api admin,eth,miner,net,txpool,personal,web3  --allow-insecure-unlock --unlock $adressAccount --password .accountpassword
     elif [[ "$NODETYPE" == "mine"* ]]; then
-        $FILE/geth --nousb --datadir=$DATADIR --bootnodes "enode://$BOOTNODEID@$BOOTNODEIP:$BOOTNODEPORT" --networkid $NETWORKID --port $MYNODEPORT     --syncmode="full"  --gasprice "0"  --etherbase $adressAccount --unlock $adressAccount --password $accountFile --mine --miner.threads 1
+        $FILE/geth --nousb --datadir=$DATADIR --bootnodes "enode://$BOOTNODEID@$BOOTNODEIP:$BOOTNODEPORT" --networkid $NETWORKID --port $MYNODEPORT     --syncmode="full"  --gasprice "0"  --etherbase $adressAccount --mine --miner.threads 1
+    elif [[ "$NODETYPE" == "boot" ]]; then
+        $FILE/geth --nousb --datadir=$DATADIR  --nodekeyhex=$BOOTNODEKEY --networkid $NETWORKID  --nat extip:$BOOTNODEIP --port $BOOTNODEPORT
     fi
     sleep 3
 elif [[ "$OPERATIONTYPE" == "stop" ]]; then
